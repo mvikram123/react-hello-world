77d484b6e203008396aef92b59868feb8307d1a9
manbodhvikram <manbodhvikram@gmail.com>
2023-07-13 17:48:38 +0530

    pros-imp

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,13 +1,28 @@
 import React from "react";
 //import  ReactDOM  from "react-dom";
-const Button=(props)=>{
-  //  props.text="guava";  // we can not do this because props is read only object/property
+// const Button=(props)=>{
+//   //  props.text="guava";  // we can not do this because props is read only object/property
 
-    return(
-        <div>
-            <button>{props.text}</button>
-            <p>This is a {props.text} which is {props.color} in color</p>
-        </div>
-    )
-}
-export default Button;
+//     return(
+//         <div>
+//             <button>{props.text}</button>
+//             <p>This is a {props.text} which is {props.color} in color</p>
+//         </div>
+//     )
+// }
+// export default Button;
+
+
+//another way 
+
+const Button=({text,color})=>{
+    //  here props={text,color}
+  
+      return(
+          <div>
+              <button>{text}</button>
+              <p>This is a {text} which is {color} in color</p>
+          </div>
+      )
+  }
+  export default Button;
